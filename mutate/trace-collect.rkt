34cb666dceffed96f1df36dd10b2c2c85520ff25
Lukas Lazarek <LukasALazarek@gmail.com>
2019-01-16 17:51:49 -0600

    trace-collect: hook in run-with-tracing to collect trace

--- mutate/trace-collect.rkt
+++ mutate/trace-collect.rkt
@@ -1,6 +1,7 @@
 #lang racket
 
-(require "mutation-runner.rkt")
+(require "mutation-runner.rkt"
+         "trace.rkt")
 
 ;; Desired data:
 ;; listof
@@ -11,7 +12,7 @@
 ;;                   Trace, Label,
 ;;                   Module, MutationIndex)
 ;;
-;; Trace := (listof Label)
+;; Trace := (hash/c Label label-bounds?)
 ;; MutationIndex := natural?
 ;; Module := Path
 ;; Bench := Path
@@ -27,10 +28,17 @@
 ;; Assume have a function that will run a given module with a given
 ;; mutation with the tracing framework and return a list of labels.
 ;; trace-of: Path Path syntax? -> Trace
-(define (trace-of main-module-path mutated-module-path mutated-module-stx)
-  '(a b c))
+(define (trace-of main-module-path
+                  mutated-module-path
+                  mutated-module-stx
+                  other-modules-to-trace)
+  (run-with-tracing main-module-path
+                    other-modules-to-trace
+                    mutated-module-path
+                    mutated-module-stx))
 
-(define (mutant-outcomes/for-modules bench main-module mutatable-modules)
+(define (mutant-outcomes/for-modules bench main-module mutatable-modules
+                                     #:report-progress [report-progress #f])
   (run-all-mutants/with-modules
    main-module
    mutatable-modules
@@ -40,15 +48,20 @@
                   mutated-module mutated-id
                   precision index)
       (define-values (mutated-stx _) (mutate-module mutated-module index))
-      (mutant-outcome bench
-                      precision
-                      (trace-of main-module
-                                mutated-module
-                                mutated-stx)
-                      outcome
-                      blamed
-                      mutated-id
-                      index)])))
+      (define this-mutant-outcome
+        (mutant-outcome bench
+                        precision
+                        (trace-of main-module
+                                  mutated-module
+                                  mutated-stx
+                                  mutatable-modules)
+                        outcome
+                        blamed
+                        mutated-id
+                        index))
+      (when report-progress
+        (displayln this-mutant-outcome))
+      this-mutant-outcome])))
 
 ;; temp: Comment out everything but one very short module
 (define benchmarks-to-mutate
@@ -57,13 +70,13 @@
                "../benchmarks/forth/untyped/eval.rkt"
                "../benchmarks/forth/untyped/stack.rkt")))
     ("snake" ("../benchmarks/snake/untyped/main.rkt"
-              (#;"../benchmarks/snake/untyped/collide.rkt"
-               #;"../benchmarks/snake/untyped/const.rkt"
+              ("../benchmarks/snake/untyped/collide.rkt"
+               "../benchmarks/snake/untyped/const.rkt"
                "../benchmarks/snake/untyped/cut-tail.rkt"
-               #;"../benchmarks/snake/untyped/data.rkt"
-               #;"../benchmarks/snake/untyped/handlers.rkt"
-               #;"../benchmarks/snake/untyped/motion-help.rkt"
-               #;"../benchmarks/snake/untyped/motion.rkt")))
+               "../benchmarks/snake/untyped/data.rkt"
+               "../benchmarks/snake/untyped/handlers.rkt"
+               "../benchmarks/snake/untyped/motion-help.rkt"
+               "../benchmarks/snake/untyped/motion.rkt")))
     #;("dungeon" ("../benchmarks/dungeon/untyped/main.rkt"
                 ("../benchmarks/dungeon/base/un-types.rkt"
                  "../benchmarks/dungeon/untyped/cell.rkt"
