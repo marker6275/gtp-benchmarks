29e7e88b5d48158118df34deff7584edff8c65f5
Lukas Lazarek <LukasALazarek@gmail.com>
2019-03-13 19:00:53 -0500

    Seperate details of precision config lattice from lattice generation

--- mutate/lattice.rkt
+++ mutate/lattice.rkt
@@ -1,56 +1,45 @@
 #lang racket
 
-(provide precision-config-lattice)
+(provide (contract-out [make-lattice
+                        (point?
+                         (point? . -> . (set/c point?))
+                         . -> .
+                         (set/c lattice-point?))])
+         (struct-out lattice-point))
 
 (module+ test
   (require ruinit))
 
-(define (precision-config-lattice modules levels)
-  (define base-level (first levels))
-  (define initial-config
-    (for/set ([mod modules])
-      (mod-config mod base-level)))
-  (make-lattice initial-config
-                (match-lambda
-                  [(mod-config m level)
-                   (define index (index-of levels level))
-                   (mod-config m (list-ref levels (add1 index)))])
-                (match-lambda
-                  [(mod-config _ level)
-                   (equal? level (last levels))])))
 
+(define point? any/c)
+(define path? (listof point?))
+;; lattice-point = point? (set/c path?)
+(struct lattice-point (value paths-to) #:transparent)
 
-;; mod-config = path-string? symbol?
-(struct mod-config (mod level) #:transparent)
-
-(define bench-config? (set/c mod-config?))
-
-(define path? (listof bench-config?))
-
-;; lattice-point = bench-config? (set/c path?)
-(struct lattice-point (config paths-to))
-
-
-(define/contract (make-lattice start increment max?)
-  (bench-config?
-   (mod-config? . -> . mod-config?)
-   (mod-config? . -> . boolean?)
-   . -> .
-   (set/c lattice-point?))
-
-  (define h (make-lattice-hash start increment max?))
-  (for/set ([(config paths) h])
-    (lattice-point config paths)))
+(define (make-lattice start parents-of)
+  (define h (make-lattice-hash start parents-of))
+  (for/set ([(point paths) h])
+    (lattice-point point paths)))
 
 
 
 
 
 (module+ test
+  (struct mod-config (mod level) #:transparent)
   (define inc (match-lambda [(mod-config m 'T) (mod-config m 'M)]
                             [(mod-config m 'N) (mod-config m 'T)]))
   (define max? (match-lambda [(mod-config m 'M) #t]
                              [_ #f]))
+  (define (parents-of point)
+    (for/fold ([parents (set)])
+              ([mod-config point])
+      (define other-mods (set-remove point mod-config))
+      (cond [(max? mod-config) parents]
+            [else
+             (define bench-config-with-mod+1
+               (set-add other-mods (inc mod-config)))
+             (set-add parents bench-config-with-mod+1)])))
 
   (define (parse-config config-as-list)
     (for/set ([mod-as-list config-as-list])
@@ -65,60 +54,25 @@
             (pretty-format actual)
             (pretty-format expected)))))
 
-(define/contract (parents-of bench-config increment max?)
-  (bench-config?
-   (mod-config? . -> . mod-config?)
-   (mod-config? . -> . boolean?)
+
+(define/contract (make-lattice-hash start parents-of)
+  (point?
+   (point? . -> . (set/c point?))
    . -> .
-   (set/c bench-config?))
+   (hash/c point? (set/c path?)))
 
-  (for/fold ([parents (set)])
-            ([mod-config bench-config])
-    (define other-mods (set-remove bench-config mod-config))
-    (cond [(max? mod-config) parents]
-          [else
-           (define bench-config-with-mod+1
-             (set-add other-mods (increment mod-config)))
-           (set-add parents bench-config-with-mod+1)])))
-
-(module+ test
-  (define-test (test-parents-of config parent-configs)
-    (define config/parsed (parse-config config))
-    (define parent-configs/parsed (list->set (map parse-config parent-configs)))
-    (test-equal? (parents-of config/parsed inc max?)
-                 parent-configs/parsed))
-
-  (test-begin
-    (test-parents-of '[(A N) (B N)]
-                     '{[(A T) (B N)]
-                       [(A N) (B T)]})
-    (test-parents-of '[(A T) (B T)]
-                     '{[(A M) (B T)]
-                       [(A T) (B M)]})
-    (test-parents-of '[(A T) (B T) (C N)]
-                     '{[(A M) (B T) (C N)]
-                       [(A T) (B M) (C N)]
-                       [(A T) (B T) (C T)]})))
-
-(define/contract (make-lattice-hash start increment max?)
-  (bench-config?
-   (mod-config? . -> . mod-config?)
-   (mod-config? . -> . boolean?)
-   . -> .
-   (hash/c bench-config? (set/c path?)))
-
-  (let loop (;; hash of bench-config? -> (set/c path?)
+  (let loop (;; hash of point? -> (set/c path?)
              [paths (hash start (set '()))]
-             ;; set of bench-config?
+             ;; set of point?
              [seen (set)]
-             ;; list of bench-config?
+             ;; list of point?
              [to-process (list start)])
     (cond [(set-empty? to-process)
            paths]
 
           [else
            (define bench (first to-process))
-           (define parents (parents-of bench increment max?))
+           (define parents (parents-of bench))
            (define parents/unseen (set->list (set-subtract parents seen)))
            (define paths+bench->parents
              (add-paths paths bench parents))
@@ -131,7 +85,7 @@
   (define-test (test-lattice start expected)
     (define start/parsed (parse-config start))
     (define expected/parsed (parse-paths-hash expected))
-    (test-path-maps=? (make-lattice-hash start/parsed inc max?)
+    (test-path-maps=? (make-lattice-hash start/parsed parents-of)
                       expected/parsed))
   (test-begin
     (test-lattice
@@ -185,27 +139,27 @@
              ([(A T) (B M)] [(A T) (B T)] [(A T) (B N)] [(A N) (B N)])
              ([(A T) (B M)] [(A T) (B T)] [(A N) (B T)] [(A N) (B N)]))))))
 
-(define/contract (add-paths paths config parent-configs)
-  ((hash/c bench-config? (set/c path?))
-   bench-config?
-   (set/c bench-config?)
+(define/contract (add-paths paths point parent-points)
+  ((hash/c point? (set/c path?))
+   point?
+   (set/c point?)
    . -> .
-   (hash/c bench-config? (set/c path?)))
+   (hash/c point? (set/c path?)))
 
-  (define config-paths (hash-ref paths config))
+  (define point-paths (hash-ref paths point))
   (for*/fold ([new-paths paths])
-             ([parent (in-set parent-configs)]
-              [config-path (in-set config-paths)])
+             ([parent (in-set parent-points)]
+              [point-path (in-set point-paths)])
     (define parent-paths (hash-ref new-paths parent (const (set))))
-    (define new-path (cons config config-path))
+    (define new-path (cons point point-path))
     (hash-set new-paths
               parent
               (set-add parent-paths new-path))))
 
 (module+ test
-  (define-test (test-add-paths paths config parent-configs new-paths)
-    (define config/parsed (parse-config config))
-    (define parent-configs/parsed (list->set (map parse-config parent-configs)))
+  (define-test (test-add-paths paths point parent-points new-paths)
+    (define config/parsed (parse-config point))
+    (define parent-configs/parsed (list->set (map parse-config parent-points)))
     (define paths/parsed (parse-paths-hash paths))
     (define new-paths/parsed (parse-paths-hash new-paths))
     (define actual (add-paths paths/parsed config/parsed parent-configs/parsed))
---
+++ mutate/precision-lattice.rkt
@@ -0,0 +1,120 @@
+#lang racket
+
+(provide (contract-out [precision-config-lattice
+                        ((listof any/c)
+                         (listof symbol?)
+                         . -> .
+                         (listof lattice-point?))]))
+
+(require "lattice.rkt")
+
+;; The precision config lattice is composed of a bunch of
+;; `bench-config`s, where each `bench-config` is a set of
+;; `mod-config`s: it represents a configuration with a setting of
+;; precision level per module.
+
+;; mod-config = path-string? symbol?
+(struct mod-config (mod level) #:transparent)
+
+(define bench-config? (set/c mod-config?))
+
+(define (precision-config-lattice modules levels)
+  (define base-level (first levels))
+  (define initial-config
+    (for/set ([mod modules])
+      (mod-config mod base-level)))
+  (make-lattice initial-config (parents-of levels)))
+
+(define/match (increment-mod-config mc levels)
+  [{(mod-config m level) levels}
+   (define index (index-of levels level))
+   (mod-config m (list-ref levels (add1 index)))])
+
+(define/match (mod-config-max? mc levels)
+  [{(mod-config _ level) levels}
+   (equal? level (last levels))])
+
+(define ((parents-of levels) bench-config)
+  (for/fold ([parents (set)])
+            ([mod-config bench-config])
+    (define other-mods (set-remove bench-config mod-config))
+    (cond [(mod-config-max? mod-config levels) parents]
+          [else
+           (define bench-config-with-mod+1
+             (set-add other-mods (increment-mod-config mod-config levels)))
+           (set-add parents bench-config-with-mod+1)])))
+
+(module+ test
+  (require ruinit)
+
+  (define (parse-config config-as-list)
+    (for/set ([mod-as-list config-as-list])
+      (mod-config (first mod-as-list) (second mod-as-list))))
+  (define (parse-paths-hash p)
+    (for/hash ([(c path-list) p])
+      (values (parse-config c)
+              (list->set (map (curry map parse-config) path-list)))))
+  (define-test (test-parents-of config parent-configs)
+    (define config/parsed (parse-config config))
+    (define parent-configs/parsed (list->set (map parse-config parent-configs)))
+    (test-equal? ((parents-of '(N T M)) config/parsed)
+                 parent-configs/parsed))
+
+  (test-begin
+    (test-parents-of '[(A N) (B N)]
+                     '{[(A T) (B N)]
+                       [(A N) (B T)]})
+    (test-parents-of '[(A T) (B T)]
+                     '{[(A M) (B T)]
+                       [(A T) (B M)]})
+    (test-parents-of '[(A T) (B T) (C N)]
+                     '{[(A M) (B T) (C N)]
+                       [(A T) (B M) (C N)]
+                       [(A T) (B T) (C T)]})
+
+    (ignore
+     (define h
+       (parse-paths-hash
+        (hash '[(A N) (B N)]
+           '(())
+
+           '[(A T) (B N)]
+           '(([(A N) (B N)]))
+
+           '[(A N) (B T)]
+           '(([(A N) (B N)]))
+
+           '[(A M) (B N)]
+           '(([(A T) (B N)] [(A N) (B N)]))
+
+           '[(A N) (B M)]
+           '(([(A N) (B T)] [(A N) (B N)]))
+
+           '[(A T) (B T)]
+           '(([(A T) (B N)] [(A N) (B N)])
+             ([(A N) (B T)] [(A N) (B N)]))
+
+           '[(A M) (B T)]
+           '(([(A M) (B N)] [(A T) (B N)] [(A N) (B N)])
+             ([(A T) (B T)] [(A T) (B N)] [(A N) (B N)])
+             ([(A T) (B T)] [(A N) (B T)] [(A N) (B N)]))
+
+           '[(A T) (B M)]
+           '(([(A N) (B M)] [(A N) (B T)] [(A N) (B N)])
+             ([(A T) (B T)] [(A T) (B N)] [(A N) (B N)])
+             ([(A T) (B T)] [(A N) (B T)] [(A N) (B N)]))
+
+           '[(A M) (B M)]
+           '(([(A M) (B T)] [(A M) (B N)] [(A T) (B N)] [(A N) (B N)])
+             ([(A M) (B T)] [(A T) (B T)] [(A T) (B N)] [(A N) (B N)])
+             ([(A M) (B T)] [(A T) (B T)] [(A N) (B T)] [(A N) (B N)])
+
+             ([(A T) (B M)] [(A N) (B M)] [(A N) (B T)] [(A N) (B N)])
+             ([(A T) (B M)] [(A T) (B T)] [(A T) (B N)] [(A N) (B N)])
+             ([(A T) (B M)] [(A T) (B T)] [(A N) (B T)] [(A N) (B N)]))))))
+    (test-equal?
+     (precision-config-lattice '(A B) '(N T M))
+     (for/set ([(point paths) h])
+       (lattice-point point paths))))
+
+  (display-test-results))
