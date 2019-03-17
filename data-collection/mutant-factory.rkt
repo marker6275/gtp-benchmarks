cb03044631079c2f310ac2e092e61b3fb1548548
Lukas Lazarek <LukasALazarek@gmail.com>
2019-03-17 12:03:41 -0500

    mutant-factory: fill in missing pieces, fix compilation errors

--- data-collection/mutant-factory.rkt
+++ data-collection/mutant-factory.rkt
@@ -2,10 +2,18 @@
 
 (require "precision-lattice.rkt"
          "benchmarks.rkt"
-         "../ctcs/current-precision-setting.rkt")
+         "../ctcs/current-precision-setting.rkt"
+         "../mutate/mutation-runner.rkt"
+         "../mutate/instrumented-module-runner.rkt"
+         racket/match
+         racket/system
+         racket/serialize
+         racket/set
+         racket/port)
 
-(define mutant-runner-path "mutant-runner.rkt")
-(define process-limit 10)
+(define mutant-runner-path (make-parameter "mutant-runner.rkt"))
+(define process-limit (make-parameter 10))
+(define temp-output-dir (make-parameter "~/mutant-data/"))
 
 (define (spawn-mutant-runner benchmark-name
                              module-to-mutate
@@ -15,7 +23,7 @@
   (define config-hash (lattice-point->config-hash lattice-point))
   (match-define (list runner-out runner-in _ runner-err runner-ctl)
     (process (format "racket '~a' -b '~a' -m '~a' -i ~a -o '~a'"
-                     mutant-runner-path
+                     (mutant-runner-path)
                      benchmark-name
                      module-to-mutate
                      mutation-index
@@ -38,6 +46,10 @@
   ;; lltodo: this might take a while, maybe report progress?
   (define config-lattice (precision-config-lattice mutatable-modules
                                                    precision-configs))
+  ;; Ensure data output directory exists
+  (unless (directory-exists? (temp-output-dir))
+    (make-directory (temp-output-dir)))
+
   (define-values (mutant-results active-mutants)
     (for/fold (;; mutant-results: mutant? -> (set mutant-process?)
                [mutant-results (hash)]
@@ -47,12 +59,15 @@
 
       (spawn-mutants/of-module module-to-mutate
                                bench-name
+                               config-lattice
                                mutant-results
                                active-mutants)))
 
   (babysit-mutants mutant-results active-mutants))
 
 ;; path-string?
+;; string?
+;; (set/c lattice-point?)
 ;; (hash/c mutant? (set/c mutant-process?))
 ;; (set/c mutant-process?)
 ;; ->
@@ -60,10 +75,9 @@
 ;; (set/c mutant-process?)
 (define (spawn-mutants/of-module module-to-mutate
                                  bench-name
+                                 config-lattice
                                  mutant-results
                                  active-mutants)
-  (define-values (_1 mod-name _2) (split-path module-to-mutate))
-
   ;; This must be an unbounded loop, number of mutants unknown
   (let next-mutant ([mutation-index 0]
                     [mutant-results mutant-results]
@@ -80,6 +94,8 @@
           [else
            (define-values (mutant-results active-mutants)
              (spawn-mutant/per-precision-in config-lattice
+                                            module-to-mutate
+                                            mutation-index
                                             bench-name
                                             mutant-results/consolidated
                                             active-mutants))
@@ -89,16 +105,21 @@
                         active-mutants)])))
 
 ;; (set/c lattice-point?)
+;; path-string?
+;; natural?
+;; string?
 ;; (hash/c mutant? (set/c mutant-process?))
 ;; (set/c mutant-process?)
 ;; ->
 ;; (hash/c mutant? (set/c mutant-process?))
 ;; (set/c mutant-process?)
 (define (spawn-mutant/per-precision-in config-lattice
+                                       module-to-mutate
+                                       mutation-index
                                        bench-name
                                        mutant-results
                                        active-mutants)
-  (for/fold ([mutant-results mutant-results/consolidated]
+  (for/fold ([mutant-results mutant-results]
              [active-mutants active-mutants]
              [active-mutant-count (set-count active-mutants)]
              #:result (values mutant-results active-mutants))
@@ -111,9 +132,11 @@
       (maybe-sweep-dead-mutants mutant-results
                                 active-mutants
                                 active-mutant-count))
-    (define outfile (build-path temp-output-dir
+    (define outfile (build-path (temp-output-dir)
                                 (format "~a__index~a_~a"
-                                        mod-name
+                                        (regexp-replace* "/"
+                                                         module-to-mutate
+                                                         "_")
                                         mutation-index
                                         i)))
     (spawn-mutant-runner bench-name
@@ -149,7 +172,7 @@
   (define active-mutant-count* (or active-mutant-count
                                    (set-count active-mutants)))
   (if (or force?
-          (>= active-mutant-count* process-limit))
+          (>= active-mutant-count* (process-limit)))
       (sweep-dead-mutants mutant-results active-mutants active-mutant-count*)
       (values mutant-results active-mutants active-mutant-count*)))
 
@@ -174,3 +197,48 @@
   (hash-set mutant-results
             mutant
             (set-add results mutant-proc)))
+
+(define (max-mutation-index-exceeded? module-to-mutate mutation-index)
+  ;; `mutate-module` throws if index is too large, so just try
+  ;; mutating to see whether or not it throws
+  (with-handlers ([mutation-index-exception? (λ _ #t)])
+    (make-instrumented-module-runner module-to-mutate
+                                     '()
+                                     (match-lambda**
+                                      [{(== module-to-mutate) stx}
+                                       (mutate-module stx mutation-index)
+                                       stx]
+                                      [{_ stx} stx]))
+    #f))
+
+(define/match (lattice-point->config-hash point)
+  [{(lattice-point bench-config _)}
+   (for/hash ([config (in-set bench-config)])
+      (match-define (mod-config mod level) config)
+      (values mod level))])
+
+;; (hash/c mutant? (set/c mutant-process?))
+;; ->
+;; (hash/c mutant? (set/c mutant-process?))
+(define (try-consolidate-mutant-results mutant-results)
+  (for/hash ([(mutant processes) (in-hash mutant-results)])
+    (if (> (set-count processes) 1)
+        (values mutant (set (consolidate-mutant-results processes)))
+        (values mutant processes))))
+
+;; (and/c (set/c mutant-process?) (count>/c 1))
+;; ->
+;; (and/c (set/c mutant-process?) (count=/c 1))
+(define (consolidate-mutant-results mutant-processes)
+  ;; Just pick an output file to be the consolidated one
+  (match-define (mutant-process _ _ consolidated-file _)
+    (set-first mutant-processes))
+  (define out (open-output-file consolidated-file #:exists 'append))
+  (for ([proc (in-set (set-rest mutant-processes))])
+    (define other-file (mutant-process-file proc))
+    (define in (open-input-file other-file))
+    (copy-port in out)
+    (newline out)
+    (close-input-port in)
+    (delete-file other-file))
+  (close-output-port out))
