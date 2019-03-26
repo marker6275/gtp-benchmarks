d6f615096905870cb7472aa84b5dfbe622234183
Lukas Lazarek <LukasALazarek@gmail.com>
2019-03-26 17:19:24 -0500

    kcfa: structs: add struct ctcs

--- benchmarks/kcfa/untyped/structs.rkt
+++ benchmarks/kcfa/untyped/structs.rkt
@@ -8,7 +8,57 @@
   (struct-out Call)
 )
 
+(provide Stx-type?
+         exp-type?
+         Ref-type?
+         Lam-type?
+         Call-type?
+         Stx/c
+         exp/c
+         Ref/c
+         Lam/c
+         Call/c)
+
+(require "../../../ctcs/precision-config.rkt"
+         racket/contract)
+
 ;; =============================================================================
+
+(define/ctc-helper (Stx/c label/c)
+  (struct/c Stx label/c))
+(define/ctc-helper (exp/c label/c)
+  (struct/c exp label/c))
+(define/ctc-helper (Ref/c label/c var/c)
+  (struct/c Ref label/c
+            var/c))
+(define/ctc-helper (Lam/c label/c formals/c call/c)
+  (struct/c Lam label/c
+            formals/c
+            call/c))
+(define/ctc-helper (Call/c label/c fun/c args/c)
+  (struct/c Call label/c
+            fun/c
+            args/c))
+(define/ctc-helper Stx-type? (Stx/c symbol?))
+(define/ctc-helper exp-type? (exp/c symbol?))
+(define/ctc-helper Ref-type? (Ref/c symbol? symbol?))
+(define/ctc-helper Lam-type? (Lam/c symbol?
+                                    (listof symbol?)
+                                    (or/c exp-type?
+                                          Ref-type?
+                                          (recursive-contract Lam-type?)
+                                          Call-type?)))
+(define/ctc-helper Call-type?
+  (Call/c symbol?
+          (or/c exp-type?
+                Ref-type?
+                Lam-type?
+                (recursive-contract Call-type?))
+          (listof (or/c exp-type?
+                        Ref-type?
+                        Lam-type?
+                        (recursive-contract Call-type?)))))
+
 
 (struct Stx
  (label ;: Symbol]))
@@ -22,6 +72,7 @@
  (formals ;: (Listof Symbol)]
   call ;: (U exp Ref Lam Call)]))
 ))
+
 (struct Call Stx
  (fun ;: (U exp Ref Lam Call)]
   args ;: (Listof (U exp Ref Lam Call))]))
