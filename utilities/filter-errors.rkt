dc7fe6060a6baf8f5b0b2ef93888b18d823020f1
Lukas Lazarek <LukasALazarek@gmail.com>
2019-03-29 14:27:31 -0500

    utils: make filter-errors not crappy
    Now it filters chunks as produced by grep with -A.
    It also prints the name of the skipped blame region.

--- utilities/filter-errors.rkt
+++ utilities/filter-errors.rkt
@@ -2,36 +2,62 @@
 
 (require racket/cmdline)
 
-(define (filter-errors log-in)
+(define (get-blamed line)
+  (second (regexp-match #rx"region \\('#\\(([^ ]+) .+at max"
+                        line)))
+(define (filter-errors log-in blames-to-ignore)
+  (define to-ignore-rx
+    (format "blamed region \\('#\\((~a).+at max.+not bug"
+            (string-join (map regexp-quote blames-to-ignore) ")|("
+                         #:before-first "("
+                         #:after-last ")")))
   (define (next-line!) (read-line log-in))
-  (let loop ()
+  (define (handle-rest-of-chunk! do-with-line)
+    (let loop ()
+      (define line (next-line!))
+      (do-with-line line)
+      (unless (or (equal? line "--") (eof-object? line))
+        (loop))))
+  (let filter-next-chunk ()
     (define line (next-line!))
-    (define next-line (next-line!))
-    (unless (or (eof-object? line) (eof-object? next-line))
+    (unless (eof-object? line)
       (define line-is-err? (regexp-match? #rx"\\* (error|fatal) \\*"
                                           line))
-      (define is-blamed@max-not-bug-err/blamed-random?
-        (regexp-match? #rx"blamed region \\('#\\(random.+at max.+not bug"
-                       next-line))
-      (cond [(and line-is-err?
-                  is-blamed@max-not-bug-err/blamed-random?)
-             (displayln "--- skipping blamed random ---")
-             ;; Skip next lines
-             (for ([_ (in-range 7)]) (next-line!))
-             (loop)]
+      (cond [line-is-err?
+             (define next-line (next-line!))
+             (define is-blamed@max-not-bug-err/blamed-random?
+               (regexp-match? to-ignore-rx next-line))
+             (cond [is-blamed@max-not-bug-err/blamed-random?
+                    (printf "--- skipping irrelevant blame: ~a ---~n"
+                            (get-blamed next-line))
+                    (handle-rest-of-chunk! void)
+                    (filter-next-chunk)]
+                   [else
+                    (displayln line)
+                    (displayln next-line)
+                    (handle-rest-of-chunk! displayln)
+                    (filter-next-chunk)])]
             [else
              (displayln line)
-             (displayln next-line)
-             (loop)]))))
+             (handle-rest-of-chunk! displayln)
+             (filter-next-chunk)]))))
 
 (module+ main
   (define log-path (make-parameter #f))
+  (define blames-to-ignore (make-parameter empty))
   (command-line
    #:once-each
    [("-f" "--log-file")
     path
     "Path to log file to filter."
-    (log-path path)])
+    (log-path path)]
+   #:args ids
+   (blames-to-ignore ids))
+  (when (empty? (blames-to-ignore))
+    (printf
+     "Error: must provide at least one blamed label to filter as arguments~n")
+    (exit 1))
+  (define filter-blames-to-ignore (curryr filter-errors (blames-to-ignore)))
   (if (log-path)
-      (call-with-input-file (log-path) filter-errors)
-      (filter-errors (current-input-port))))
+      (call-with-input-file (log-path) filter-blames-to-ignore)
+      (filter-blames-to-ignore (current-input-port))))
