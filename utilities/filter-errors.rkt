a64b7fbf27ab3a8228897624cb03a5edb4e49c54
Lukas Lazarek <LukasALazarek@gmail.com>
2019-03-31 13:01:33 -0500

    utilities: add extra filters to filter-errors

--- utilities/filter-errors.rkt
+++ utilities/filter-errors.rkt
@@ -5,7 +5,7 @@
 (define (get-blamed line)
   (second (regexp-match #rx"region \\('#\\(([^ ]+) .+at max"
                         line)))
-(define (filter-errors log-in blames-to-ignore)
+(define (filter-errors log-in blames-to-ignore extra-filters)
   (define to-ignore-rx
     (format "blamed region \\('#\\((~a).+at max.+not bug"
             (string-join (map regexp-quote blames-to-ignore) ")|("
@@ -23,41 +23,53 @@
     (unless (eof-object? line)
       (define line-is-err? (regexp-match? #rx"\\* (error|fatal) \\*"
                                           line))
-      (cond [line-is-err?
-             (define next-line (next-line!))
-             (define is-blamed@max-not-bug-err/blamed-random?
-               (regexp-match? to-ignore-rx next-line))
-             (cond [is-blamed@max-not-bug-err/blamed-random?
-                    (printf "--- skipping irrelevant blame: ~a ---~n"
-                            (get-blamed next-line))
-                    (handle-rest-of-chunk! void)
-                    (filter-next-chunk)]
-                   [else
-                    (displayln line)
-                    (displayln next-line)
-                    (handle-rest-of-chunk! displayln)
-                    (filter-next-chunk)])]
-            [else
-             (displayln line)
-             (handle-rest-of-chunk! displayln)
-             (filter-next-chunk)]))))
+      (define chunk-remainder-handler
+        (cond [line-is-err?
+               (define next-line (next-line!))
+               (define is-blamed@max-not-bug-err/blamed-random?
+                 (regexp-match? to-ignore-rx next-line))
+               (define extra-filter-matches?
+                 (ormap (curryr regexp-match? next-line) extra-filters))
+               (cond [is-blamed@max-not-bug-err/blamed-random?
+                      (printf "--- skipping irrelevant blame: ~a ---~n"
+                              (get-blamed next-line))
+                      void]
+                     [extra-filter-matches?
+                      (displayln "--- skipping due to extra filter ---")
+                      void]
+                     [else
+                      (displayln line)
+                      (displayln next-line)
+                      displayln])]
+              [else
+               (displayln line)
+               displayln]))
+      (handle-rest-of-chunk! chunk-remainder-handler)
+      (filter-next-chunk))))
 
 (module+ main
   (define log-path (make-parameter #f))
   (define blames-to-ignore (make-parameter empty))
+  (define extra-filters (make-parameter empty))
   (command-line
    #:once-each
    [("-f" "--log-file")
     path
-    "Path to log file to filter."
+    "Path to log file to filter. Otherwise filter stdin."
     (log-path path)]
+   #:multi
+   [("-e" "--extra-filter")
+    rx
+    "Chunks matching this regexp will also be filtered out."
+    (extra-filters (cons rx (extra-filters)))]
    #:args ids
    (blames-to-ignore ids))
   (when (empty? (blames-to-ignore))
     (printf
      "Error: must provide at least one blamed label to filter as arguments~n")
     (exit 1))
-  (define filter-blames-to-ignore (curryr filter-errors (blames-to-ignore)))
+  (define filter-blames-to-ignore
+    (curryr filter-errors (blames-to-ignore) (extra-filters)))
   (if (log-path)
       (call-with-input-file (log-path) filter-blames-to-ignore)
       (filter-blames-to-ignore (current-input-port))))
